baf4c179202643416d3f29452910ccd3113054e9
Constantine-Medved <medved.k@outlook.com>
2023-03-12 22:51:12 +0300

    Tasks 1 - 2 completed

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,47 @@ public class Main {
         task3();
         task4();
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
     public static void task1() {
         System.out.println("Задача 1.");
+        int[ ] arrForSum = generateRandomArray();
+        int sum = 0;
+        for (int index : arrForSum) {
+            sum += index;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
     }
     public static void task2() {
         System.out.println("Задача 2.");
+        int[ ] arrForMinMax = generateRandomArray();
+        int sumMonthExpenses = 0;
+        for (int dailyExpenses : arrForMinMax) {
+            sumMonthExpenses += dailyExpenses;
+        }
+        int minDailyExpenses = sumMonthExpenses;
+        int maxDailyExpenses = 0;
+        for (int dailyExpenses : arrForMinMax) {
+            if (dailyExpenses > maxDailyExpenses) {
+                maxDailyExpenses = dailyExpenses;
+            }
+        }
+        for (int dailyExpenses : arrForMinMax) {
+            if (minDailyExpenses > dailyExpenses) {
+                minDailyExpenses = dailyExpenses;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minDailyExpenses + " рублей. " + "Максимальная сумма трат за день составила " + maxDailyExpenses + " рублей.");
     }
     public static void task3() {
         System.out.println("Задача 3.");
-    }
+        }
     public static void task4() {
         System.out.println("Задача 4.");
     }
